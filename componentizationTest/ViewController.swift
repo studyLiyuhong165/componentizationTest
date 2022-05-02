7164a56cdfcb62258fae1ede529fb6d30d7924d4
studyLiyuhong165 <study_liyuhong165@163.com>
2022-05-02 15:19:19 +0800

    初始化项目

---
+++ componentizationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  componentizationTest
+//
+//  Created by 李宇鸿 on 2022/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
